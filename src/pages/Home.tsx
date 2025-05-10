5c3333f6ef92c569d5d80a04dd9b45b281b71ba9
Mohit Sahu <125088141+mohitsahu21@users.noreply.github.com>
2025-05-10 21:37:15 +0530

    create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,39 @@
-import React from 'react'
-import { TypeAnimation } from 'react-type-animation';
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import Cards from "../components/card/Cards";
 
 const Home = () => {
   return (
     <>
-    <div className='flex justify-center items-center flex-col pt-5'>
+      <div className="flex justify-center items-center flex-col pt-5">
         <div>
-    <h1 className='text-5xl'>Welcome to Interview Adda</h1>
-    </div>
-    <div className='mt-6'>
-         <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'We provide interview questions for Javascript',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'We provide interview questions for React.js',
-        1000,
-        'We provide interview questions for Node.js',
-        1000,
-        'We provide interview questions for HTML',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
-      repeat={Infinity}
-    />
-    </div>
-
-    </div>
-   
+          <h1 className="text-4xl">Welcome to Interview Adda</h1>
+        </div>
+        <div className="mt-6">
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              "We provide interview questions for Javascript",
+              1000, // wait 1s before replacing "Mice" with "Hamsters"
+              "We provide interview questions for React.js",
+              1000,
+              "We provide interview questions for Node.js",
+              1000,
+              "We provide interview questions for HTML",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: "3em", display: "inline-block" }}
+            repeat={Infinity}
+          />
+        </div>
+      </div>
+      <div>
+        <Cards />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
