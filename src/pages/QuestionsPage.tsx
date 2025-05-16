925ee861c1ff1fd25cd76581adbe2357b492befc
Mohit Sahu <125088141+mohitsahu21@users.noreply.github.com>
2025-05-16 21:40:11 +0530

    create question page

---
+++ src/pages/QuestionsPage.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+
+type Question = {
+  id: string;
+  question: string;
+  answer: string;
+};
+
+type QuestionsPageProps = {
+  questions: Question[];
+};
+
+const QuestionsPage = ({questions} : QuestionsPageProps) => {
+    const [data, setData] = useState<Question[]>();
+    
+   useEffect(() => {
+  if (questions.length > 0) {
+    setData(questions);
+  }
+}, [questions]); // include `questions` as a dependency
+  return (
+   <div>
+    {data?.map((item)=>(
+        <div key={item.id}>
+            <h3>{item.question}</h3>
+            <p>{item.answer}</p>
+
+        </div>
+    ))}
+   </div>
+  )
+}
+
+export default QuestionsPage
