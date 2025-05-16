84126afc6c61104ec4146e42936afc39dc714372
Mohit Sahu <125088141+mohitsahu21@users.noreply.github.com>
2025-05-17 01:10:55 +0530

    some changes

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Accordion, Span } from "@chakra-ui/react"
 type Question = {
   id: string;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { topics } from "../Constants/topics";
 import QuestionsPage from "../pages/QuestionsPage";
 import Home from "../pages/Home";
