eaf2bc9984696e32cd01c36b2f94b5f4820c1178
Mohit Sahu <125088141+mohitsahu21@users.noreply.github.com>
2025-05-16 21:37:42 +0530

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import AppRoutes from './routes/router'
 
 function App() {
 
   return (
     <>
       
-      <Home/>
+      {/* <Home/> */}
+      <AppRoutes/>
       
 
     </>
